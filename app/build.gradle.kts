659ee8717ca36857ceb57a03b6c5de3dd7b0fcad
amarjeetmaan3 <amarjeetmaan33@gmail.com>
2026-08-31 09:33:30 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,32 +6,15 @@ plugins {
 val runNumber = System.getenv("GITHUB_RUN_NUMBER")?.toIntOrNull() ?: 1
 
 android {
-    // इसे वापस ओरिजिनल कर दिया गया है ताकि R फाइल सही जगह बने
     namespace = "com.mblivestudio"
     compileSdk = 34
 
-    // STATIC KEYSTORE CONFIGURATION
-    signingConfigs {
-        create("release") {
-            keyAlias = "mblive"
-            keyPassword = "mblivepassword"
-            storePassword = "mblivepassword"
-        }
-    }
-
     defaultConfig {
-        // इसे v2 रखा गया है ताकि बिना अनइंस्टॉल किए ऐप अपडेट/इंस्टॉल हो सके
         applicationId = "com.mblivestudio.v2"
         minSdk = 24
         targetSdk = 34
         versionCode = runNumber
         versionName = "2.0.$runNumber"
-    }
-
-    buildTypes {
-        getByName("debug") {
-            // signingConfig = signingConfigs.getByName("release") 
-        }
     }
 
     compileOptions {
