07403997f25c44ba7f0be032d2ae8bcbda67f47c
amarjeetmaan3 <amarjeetmaan33@gmail.com>
2026-09-08 20:53:26 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,10 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.activity:activity-ktx:1.8.2")
-    implementation("io.github.thibaultbee:streampack:2.4.1")
+    
+    // Updated StreamPack 2.9.0 Split Dependencies
+    implementation("io.github.thibaultbee.streampack:streampack-core:2.9.0")
+    implementation("io.github.thibaultbee.streampack:streampack-extension-rtmp:2.9.0")
 
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.api-client:google-api-client-android:1.32.2")
