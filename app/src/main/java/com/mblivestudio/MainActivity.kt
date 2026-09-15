3ab84d20fc776728a31d7035c8799fafeb3079e6
amarjeetmaan3 <amarjeetmaan33@gmail.com>
2026-09-15 22:36:23 +0530

    Update MainActivity.kt

--- app/src/main/java/com/mblivestudio/MainActivity.kt
+++ app/src/main/java/com/mblivestudio/MainActivity.kt
@@ -239,11 +239,11 @@ class MainActivity : Activity(), ConnectChecker, SurfaceHolder.Callback {
         }
         window.decorView.systemUiVisibility = (
             View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG.LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG.LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG.LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG.HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG.FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
         )
 
         setContentView(R.layout.activity_main)
